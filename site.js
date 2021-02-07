4c43fc9d90739e35a54855309a4ea32177b84779
vaalkor <groggydoggie@protonmail.com>
2021-02-07 02:02:20 +0000

    Update

---
+++ site.js
@@ -0,0 +1,30 @@
+'use strict'
+
+fetch('coindata.json')
+.then(response => response.json())
+.then(data => drawGraphs(data));
+
+function drawGraphs(coins)
+{
+  let body = document.getElementsByTagName("body")[0];
+
+  Object.keys(coins).forEach(x => {
+    console.log(x);
+    console.log(coins[x])
+
+    let text = document.createElement('p');
+    text.innerText = `${x} (${coins[x]})`;
+    body.appendChild(text);
+
+    let graphDiv = document.createElement('div');
+    graphDiv.style="width:95vw";
+    text.innerText = `${x} (${coins[x]})`; 
+    body.appendChild(graphDiv);
+
+    new Dygraph(
+      graphDiv,
+      `${x}.csv`
+    );
+
+  });
+}
